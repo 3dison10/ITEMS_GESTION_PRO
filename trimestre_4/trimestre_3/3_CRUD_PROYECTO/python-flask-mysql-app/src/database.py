c87f702bf315c0e79a37f433682dad5032a73a65
Cristhian <fercho180497@gmail.com>
2024-06-24 19:00:51 -0500

    mas cambios

--- trimestre_4/trimestre_3/3_CRUD_PROYECTO/python-flask-mysql-app/src/database.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector
-
-database = mysql.connector.connect(
-    host='192.168.0.32',
-    user='itemsGestion',
-    password='Colombia2024',
-    database='items'
-)
--- trimestre_4/trimestre_4/ItemsPro/itemsproapp/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import cliente
-
-class clienteForm(forms.ModelForm):
-    class Meta:
-        model = cliente
-        fields = '__all__'
